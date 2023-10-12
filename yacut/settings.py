5ec71ea6d6921f8e7ae3308adddac6f702f81745
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2023-10-12 17:44:30 +0300

    settings.py refactored

--- yacut/settings.py
+++ yacut/settings.py
@@ -3,7 +3,10 @@ import string
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URI",
+        default='sqlite:///db.sqlite3'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
 
